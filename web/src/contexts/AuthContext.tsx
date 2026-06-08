e510124628d2cc1e1cfe25b381458ea288560f14
Paulo Neto <paulo_bneto@hotmail.com>
2026-06-08 14:53:38 -0300

    context component created

---
+++ web/src/contexts/AuthContext.tsx
@@ -0,0 +1,17 @@
+import { createContext, ReactNode, useState } from "react";
+
+type AuthContext = {
+  session: null | UserAPIResponse
+}
+
+export const AuthContext = createContext({} as AuthContext);
+
+export function AuthProvider({children}: {children: ReactNode}) {
+  const [session, setSession ]= useState<null | UserAPIResponse>(null)
+
+  return (
+    <AuthContext.Provider value={{session}}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,32 +1,33 @@
 import { BrowserRouter } from "react-router";
 
+import { Loading } from "../components/Loading";
+import { useAuth } from "../hooks/useAuth";
 import { AuthRoutes } from "./AuthRoutes";
 import { EmployeeRoutes } from "./EmployeeRoutes";
 import { ManagerRoutes } from "./ManagerRoutes";
 
-const isLoading = true;
-// const session = undefined
-
-const session = {
-  user: {
-    role: "",
-  },
-};
-
-export function Route() {
-  switch (session?.user.role) {
-    case "employee":
-      return <EmployeeRoutes />;
-
-    case "manager":
-      return <ManagerRoutes />;
-
-    default:
-      return <AuthRoutes />;
-  }
-}
+const isLoading = false;
 
 export function Routes() {
+  const {session} = useAuth();
+
+  function Route() {
+    switch (session?.user.role) {
+      case "employee":
+        return <EmployeeRoutes />;
+
+      case "manager":
+        return <ManagerRoutes />;
+
+      default:
+        return <AuthRoutes />;
+    }
+  }
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <BrowserRouter>
       <Route />
